15e9c2d553338f49c824cd5fb97eac4447b97431
Jeffrey Meyer itotallyrock <itotallyrock@users.noreply.github.com>
2017-08-08 01:46:36 -0500

    Add api route to router
    This will be the outward facing interface for either rest or graphql
    apis.

--- src/router.js
+++ src/router.js
@@ -1,7 +1,9 @@
 const { Router } = require('express')
 const index = require('./routes/index')
+const api = require('./routes/api')
 
 let router = Router({ mergerParams: true })
 router.use('/', index)
+router.use('/api', api)
 
 module.exports = router
--- src/routes/api.js
+++ src/routes/api.js
@@ -2,7 +2,9 @@ const { Router } = require('express')
 
 let router = Router({ mergerParams: true })
 router.get('/', (request, response) => {
-  response.status(200).send('Success')
+  response.status(200).json({
+    status: 'ok'
+  })
 })
 
 module.exports = router
