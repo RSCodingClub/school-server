94d6027b4419e083abece1103c0a4b256ee0a9ff
Jeffrey Meyer itotallyrock <itotallyrock@users.noreply.github.com>
2017-08-10 22:10:23 -0500

    Convert existing log calls to npmlog.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const log = require('npmlog')
 const express = require('express')
 const router = require('./router')
 
@@ -9,5 +10,5 @@ app.use('/', router)
 app.listen(PORT || 3000, function () {
   let { address, port } = this.address()
   // eslint-disable-next-line no-console
-  console.log(`Server listening at ${address}${port}`)
+  log.info('server', 'listening at %s:%d', address, port)
 })
