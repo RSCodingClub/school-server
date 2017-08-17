f954b359a2934e2fc4a2f40e95f17f8d1388b184
itotallyrock <itotallyrock@users.noreply.github.com>
2017-08-17 16:43:00 -0500

    User isRegistered method.
    This is to check if the user has any information stored in Redis and
    return a boolean based off of this.

--- src/classes/User.js
+++ src/classes/User.js
@@ -5,6 +5,14 @@ const User = class User {
   constructor (userId) {
     this.id = userId
   }
+  isRegistered () {
+    return new Promise((resolve, reject) => {
+      redisClient.exists(`user:${this.id}`, (err, exists) => {
+        if (err) return reject(err)
+        return resolve(Boolean(Number(exists)))
+      })
+    })
+  }
 }
 
 module.exports = User
