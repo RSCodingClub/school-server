f01bdfc1909792a8a24cc7191a7513a2cfc991ce
itotallyrock <itotallyrock@users.noreply.github.com>
2017-12-12 19:28:49 -0600

    Convert all User to use promises and add Rabbit Messages for User.
    Converted all existing features to promises and made functions in User
    async.  For mutators in User, send out a rabbit message, this will allow
    other services to update their own data.

--- src/classes/User.js
+++ src/classes/User.js
@@ -1,4 +1,5 @@
 
+const { promisify } = require('util')
 const redisClient = require('../redis')
 const amqplib = require('../amqplib')
 
@@ -7,87 +8,124 @@ const User = class User {
     this.id = userId
     this.amqpChannel = amqplib.getChannel()
   }
-  isRegistered () {
-    return new Promise((resolve, reject) => {
-      redisClient.exists(`user:${this.id}:name`, (err, exists) => {
-        if (err) return reject(err)
-        return resolve(Boolean(Number(exists)))
-      })
-    })
+  async isRegistered () {
+    let exists = await promisify(redisClient.exists).call(redisClient, `user:${this.id}:name`)
+    return Boolean(Number(exists))
   }
-  getName () {
-    return new Promise((resolve, reject) => {
-      redisClient.hget(`user:${this.id}:name`, (err, name) => {
-        if (err) return reject(err)
-        return resolve(name.toString())
-      })
-    })
+  async getName () {
+    return promisify(redisClient.get).call(redisClient, `user:${this.id}:name`)
   }
-  setName (newName) {
-    return new Promise((resolve, reject) => {
-      redisClient.set(`user:${this.id}:name`, newName.toString().trim(), (err, name) => {
-        if (err) return reject(err)
-        return resolve(name)
-      })
-    })
+  async setName (newName) {
+    await promisify(redisClient.set).call(redisClient, `user:${this.id}:name`, newName.toString().trim())
+
+    let channel = await this.amqpChannel
+    await channel.assertQueue('user:name:updated', { durable: true })
+    channel.sendToQueue('user:name:updated', Buffer.from(JSON.stringify({
+      id: this.id,
+      name: newName
+    })))
+
+    return newName
   }
-  giveBadge (badgeId) {
-    return new Promise((resolve, reject) => {
-      redisClient.sadd(`user:${this.id}:badges`, badgeId, (err) => {
-        if (err) return reject(err)
-        return resolve()
-      })
-    })
+  async getEmail () {
+    return promisify(redisClient.get).call(redisClient, `user:${this.id}:email`)
   }
-  takeBadge (badgeId) {
-    return new Promise((resolve, reject) => {
-      redisClient.srem(`user:${this.id}:badges`, badgeId, (err) => {
-        if (err) return reject(err)
-        return resolve()
-      })
-    })
+  async setEmail (newEmail) {
+    await promisify(redisClient.set).call(redisClient, `user:${this.id}:email`, newEmail.toString().trim())
+
+    let channel = await this.amqpChannel
+    await channel.assertQueue('user:email:updated', { durable: true })
+    channel.sendToQueue('user:email:updated', Buffer.from(JSON.stringify({
+      id: this.id,
+      email: newEmail
+    })))
+
+    return newEmail
   }
-  hasBadge (badgeId) {
-    return new Promise((resolve, reject) => {
-      redisClient.sismember(`user:${this.id}:badges`, badgeId, (err, hasBadge) => {
-        if (err) return reject(err)
-        return resolve(Boolean(hasBadge || 0))
-      })
-    })
+  async getImage () {
+    return promisify(redisClient.get).call(redisClient, `user:${this.id}:image`)
   }
-  getBadges () {
-    return new Promise((resolve, reject) => {
-      redisClient.smembers(`user:${this.id}:badges`, (err, badgesSet) => {
-        if (err) return reject(err)
-        return resolve(badgesSet.map(badge => Number(badge)) || [])
-      })
-    })
+  async setImage (newImage) {
+    await promisify(redisClient.set).call(redisClient, `user:${this.id}:image`, newImage.toString().trim())
+
+    let channel = await this.amqpChannel
+    await channel.assertQueue('user:image:updated', { durable: true })
+    channel.sendToQueue('user:image:updated', Buffer.from(JSON.stringify({
+      id: this.id,
+      image: newImage
+    })))
+
+    return newImage
   }
-  addScore (score) {
-    return new Promise((resolve, reject) => {
-      if (isNaN(Number(score))) return reject(new Error('Score must be a number'))
-      redisClient.incrby(`user:${this.id}:score`, score, (err) => {
-        if (err) return reject(err)
-        return resolve()
-      })
-    })
+  async giveBadge (badgeId) {
+    await promisify(redisClient.sadd).call(redisClient, `user:${this.id}:badges`, badgeId)
+
+    let channel = await this.amqpChannel
+    await channel.assertQueue('user:badges:updated', { durable: true })
+    channel.sendToQueue('user:badges:updated', Buffer.from(JSON.stringify({
+      id: this.id,
+      removed: [],
+      added: badgeId instanceof Array ? badgeId : [ badgeId ]
+    })))
+
+    // NOTE: Maybe return all user badges, or those added
   }
-  setScore (score) {
-    return new Promise((resolve, reject) => {
-      if (isNaN(Number(score))) return reject(new Error('Score must be a number'))
-      redisClient.set(`user:${this.id}:score`, score, (err, score) => {
-        if (err) return reject(err)
-        return resolve(Number(score || 0))
-      })
-    })
+  async takeBadge (badgeId) {
+    await promisify(redisClient.srem).call(redisClient, `user:${this.id}:badges`, badgeId)
+
+    let channel = await this.amqpChannel
+    await channel.assertQueue('user:badges:updated', { durable: true })
+    channel.sendToQueue('user:badges:updated', Buffer.from(JSON.stringify({
+      id: this.id,
+      removed: badgeId instanceof Array ? badgeId : [ badgeId ],
+      added: []
+    })))
+
+    // NOTE: Maybe return all user badges, or those added
   }
-  getScore () {
-    return new Promise((resolve, reject) => {
-      redisClient.get(`user:${this.id}:score`, (err, score) => {
-        if (err) return reject(err)
-        return resolve(Number(score || 0))
-      })
-    })
+  async hasBadge (badgeId) {
+    let hasBadge = await promisify(redisClient.sismember).call(redisClient, `user:${this.id}:badges`, badgeId)
+    return Boolean(Number(hasBadge))
+  }
+  async getBadges () {
+    let badges = await promisify(redisClient.smembers).call(redisClient, `user:${this.id}:badges`)
+    return badges.map(Number)
+  }
+  async addScore (scoreIncr) {
+    if (isNaN(scoreIncr)) throw new Error('Score must be a number')
+    let score = await promisify(redisClient.incrby).call(redisClient, `user:${this.id}:score`, scoreIncr)
+
+    let channel = await this.amqpChannel
+    await channel.assertQueue('user:score:updated', { durable: true })
+    channel.sendToQueue('user:score:updated', Buffer.from(JSON.stringify({
+      id: this.id,
+      score: score
+    })))
+
+    // NOTE: Maybe return new score
+  }
+  async setScore (score) {
+    if (isNaN(score)) throw new Error('Score must be a number')
+    await promisify(redisClient.set).call(redisClient, `user:${this.id}:score`, score)
+
+    let channel = await this.amqpChannel
+    await channel.assertQueue('user:score:updated', { durable: true })
+    channel.sendToQueue('user:score:updated', Buffer.from(JSON.stringify({
+      id: this.id,
+      score: score
+    })))
+
+    // NOTE: Maybe return new score
+  }
+  async getScore () {
+    let score = await promisify(redisClient.get).call(redisClient, `user:${this.id}:score`)
+    if (isNaN(score)) return 0
+    return Number(score)
+  }
+  async getLeaderboardIndex () {
+    let rank = await promisify(redisClient.zrevrank).call(redisClient, 'leaderboard', this.id)
+    if (isNaN(rank)) return -1
+    return Number(rank)
   }
 }
 
