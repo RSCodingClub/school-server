a44cd7adf2bc8d6fa6803724bab8d5387a790183
itotallyrock <itotallyrock@users.noreply.github.com>
2017-12-23 04:35:12 -0600

    Add mergeTypes to buildSchema.
    Will automatically merge all schemas in the schemas folder before
    building.

--- src/rootSchema.js
+++ src/rootSchema.js
@@ -1,11 +1,13 @@
 
+const path = require('path')
 const { buildSchema } = require('graphql')
+const { fileLoader, mergeTypes } = require('merge-graphql-schemas')
 
-let schema = buildSchema(`
-  type Query {
-    status: String
-  }
-`)
+let schema = buildSchema(
+  mergeTypes(
+    fileLoader(path.join(__dirname, './graphql/schemas'))
+  )
+)
 
 let resolver = {
   // TODO: Return server status
