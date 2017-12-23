d13e4314d85190c4e0f4c4e5a44c1275f6405506
itotallyrock <itotallyrock@users.noreply.github.com>
2017-12-23 04:36:37 -0600

    Enable checking for graphiql, so it only runs during development.

--- src/routes/api.js
+++ src/routes/api.js
@@ -95,7 +95,7 @@ router.get('/', (request, response) => {
 router.use('/graphql', graphqlHTTP({
   schema: rootSchema.schema,
   rootValue: rootSchema.resolver,
-  graphiql: true
+  graphiql: process.env.NODE_ENV === 'development'
 }))
 
 module.exports = router
